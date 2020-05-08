82b8a0194921e3d6b7c56993d15d504fcef15cfc
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2020-05-08 15:07:24 -0400

    List accounts on Accounts screen

--- lib/screens/accounts/screen.dart
+++ lib/screens/accounts/screen.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class AccountsScreen extends StatelessWidget {
+import '../../database.dart';
+import 'components/account_list_item.dart';
+
+class AccountsScreen extends HookWidget {
   @override
   Widget build(BuildContext context) {
+    final accounts = useState<List<Account>>([
+      Account(id: 1, name: 'Account 1', typeID: 1),
+      Account(id: 2, name: 'Account 2', typeID: 1),
+      Account(id: 3, name: 'Account 3', typeID: 1),
+    ]);
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('MoneyMan'),
+        title: const Text('Accounts'),
       ),
-      body: const Text('Hello world'),
+      body: ListView.builder(
+        itemCount: accounts.value.length,
+        itemBuilder: (ctx, i) {
+          return _buildAccountRow(context, accounts.value[i]);
+        },
+      ),
+    );
+  }
+
+  Widget _buildAccountRow(BuildContext context, Account account) {
+    return InkWell(
+      onTap: () => Navigator.pushNamed(context, '/account', arguments: account),
+      child: AccountListItem(account: account, balance: '\$ 1270.39'),
     );
   }
 }
