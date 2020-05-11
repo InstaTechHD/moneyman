8b504557482487556def824986e9be5ffe9bf352
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2020-05-11 19:01:55 -0400

    Decrease initial Linux window size

--- linux/window_configuration.cc
+++ linux/window_configuration.cc
@@ -1,5 +1,5 @@
 #include "window_configuration.h"
 
 const char *kFlutterWindowTitle = "moneyman";
-const unsigned int kFlutterWindowWidth = 800;
+const unsigned int kFlutterWindowWidth = 400;
 const unsigned int kFlutterWindowHeight = 600;
