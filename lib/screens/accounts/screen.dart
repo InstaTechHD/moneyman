3380951920a625219f643ba131084450ff2b9422
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2020-05-11 11:29:17 -0400

    Get accounts from service

--- lib/screens/accounts/screen.dart
+++ lib/screens/accounts/screen.dart
@@ -1,28 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:provider/provider.dart';
 
 import '../../database.dart';
+import '../../services/accounts.dart';
 import 'components/account_list_item.dart';
 
 class AccountsScreen extends HookWidget {
   @override
   Widget build(BuildContext context) {
-    final accounts = useState<List<Account>>([
-      Account(id: 1, name: 'Account 1', typeID: 1),
-      Account(id: 2, name: 'Account 2', typeID: 1),
-      Account(id: 3, name: 'Account 3', typeID: 1),
-    ]);
+    final accounts = useFuture(useMemoized(
+      () => Provider.of<AccountsService>(context).getAccounts(),
+    ));
+
+    Widget body;
+    if (accounts.connectionState != ConnectionState.done || accounts.hasError) {
+      body = Container();
+    } else {
+      body = ListView.builder(
+        itemCount: accounts.data.length,
+        itemBuilder: (ctx, i) {
+          return _buildAccountRow(context, accounts.data[i]);
+        },
+      );
+    }
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Accounts'),
       ),
-      body: ListView.builder(
-        itemCount: accounts.value.length,
-        itemBuilder: (ctx, i) {
-          return _buildAccountRow(context, accounts.value[i]);
-        },
-      ),
+      body: body,
     );
   }
 
