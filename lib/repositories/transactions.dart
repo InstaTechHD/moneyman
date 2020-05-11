37c117939869100cf2cdaf953b7f9bda1b06ca10
Fredrik Blomqvist <fredrik.blomqvist.95@gmail.com>
2020-05-11 11:26:22 -0400

    Add transactions repo

---
+++ lib/repositories/transactions.dart
@@ -0,0 +1,26 @@
+import '../daos/transactions.dart';
+import '../database.dart';
+import 'repository.dart';
+
+abstract class TransactionsRepository extends Repository<TXN, int> {}
+
+class TransactionsLocalRepository implements TransactionsRepository {
+  final TransactionDao dao;
+
+  TransactionsLocalRepository(AppDatabase db) : dao = TransactionDao(db);
+
+  @override
+  Future<int> create(TXN transaction) => dao.insertTransaction(transaction);
+
+  @override
+  Future delete(TXN transaction) => dao.deleteTransaction(transaction);
+
+  @override
+  Future<TXN> get(int id) => dao.getTransaction(id);
+
+  @override
+  Future<List<TXN>> getAll() => dao.getAllTransactions();
+
+  @override
+  Future update(TXN transaction) => dao.updateTransaction(transaction);
+}
